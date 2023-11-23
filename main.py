1b8a391b2a533b2cfb17057c862d01b74a7c26bb
Regidan-codes <abhinadiger@gmail.com>
2023-11-23 10:33:37 +0530

    Render the app in two columns

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=600)
+    st.image("images/photo.png")
 
 with col2:
     st.title("Abhishek N")
@@ -20,3 +21,15 @@ content2 = """
 Below are some of the apps I've built in python. Feel free to contact me for more info.
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
